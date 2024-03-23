884a5a9a49fdcc8ed2c496ec43a32be357a12a9f
dstrick17 <117781439+dstrick17@users.noreply.github.com>
2024-03-23 16:13:01 -0400

    Update kaplan-delta-mpap.py

--- kaplan-delta-mpap.py
+++ kaplan-delta-mpap.py
@@ -20,10 +20,12 @@ df_small_change_mPAP = df[df['∆ mPAP'] <= 5]
 df_big_change_mPAP = df[df['∆ mPAP'] > 5]
 
 # Create Kaplan-Meier estimators for each group
-kmf_small_change_mPAP = KaplanMeierFitter()
-kmf_big_change_mPAP = KaplanMeierFitter()
+kmf = KaplanMeierFitter() # All patients
+kmf_small_change_mPAP = KaplanMeierFitter() # small drop in mPAP
+kmf_big_change_mPAP = KaplanMeierFitter() # big drop in mPAP
 
 # Fit estimators to the data for each group
+kmf.fit(durations=df["Time in months"], event_observed=df["event_occurred"])
 kmf_small_change_mPAP.fit(durations=df_small_change_mPAP["Time in months"], event_observed=df_small_change_mPAP["event_occurred"])
 kmf_big_change_mPAP.fit(durations=df_big_change_mPAP["Time in months"], event_observed=df_big_change_mPAP["event_occurred"])
 
@@ -34,6 +36,7 @@ results = logrank_test(durations_A=df_small_change_mPAP["Time in months"],
                        event_observed_B=df_big_change_mPAP["event_occurred"])
 
 # Plot the Kaplan-Meier curves for each group
+kmf.plot_survival_function(label=f'All Patients (n={len(df)})')
 kmf_small_change_mPAP.plot_survival_function(label=f'Reduction in mPAP by ≤ 5 Wood Units (n={len(df_small_change_mPAP)})')
 kmf_big_change_mPAP.plot_survival_function(label=f'Reduction in mPAP by > 5 Wood Units (n={len(df_big_change_mPAP)})')
 
@@ -44,7 +47,8 @@ plt.ylabel("Survival Probability")
 plt.legend()
 
 # Annotate the log-rank p-value on the plot
-plt.text(60, 0.5, f'Log-rank p-value: {results.p_value:.4f}', ha='left', va='center', color='red', fontsize=12)
+plt.text(100, 0.5, f'Log-rank p-value: {results.p_value:.4f}', ha='left', va='center', color='red', fontsize=12)
 
 # Show the plot
 plt.show()
+
